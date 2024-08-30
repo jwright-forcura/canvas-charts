d9857ac7e059b60f4443b8d6ec37b8fe1840a98f
jwright-forcura <137829164+jwright-forcura@users.noreply.github.com>
2024-08-30 16:01:09 -0400

    More stuff (#2)
    * Improve pie chart animation.

--- src/Charts/PieChart/index.jsx
+++ src/Charts/PieChart/index.jsx
@@ -4,24 +4,10 @@ const PieChart = ({data, width, height, radius, lineWidth, fontSize}) => {
     const canvasRef = useRef(null);
 
     useEffect(() => {
-        if (canvasRef.current) {
-            
+        if (canvasRef.current) {            
             const canvas = canvasRef.current;
             const ctx = canvas.getContext("2d");
             const totalRecords = data.reduce((accumulator, currentValue) => accumulator + currentValue.count, 0);
-
-            // print text
-            ctx.fillstyle = "black";
-            ctx.font = `${fontSize}px sans-serif`;
-            const text = totalRecords.toString();
-            const { width, actualBoundingBoxAscent, actualBoundingBoxDescent } = ctx.measureText(text);
-            const offsetWidth = width / 2;
-            const offsetHeight = (actualBoundingBoxAscent + actualBoundingBoxDescent) / 2;
-            const textX = (canvas.width / 2) - offsetWidth;
-            const textY = (canvas.height / 2) + offsetHeight;
-            ctx.fillText(text, textX, textY);
-
-
             const xMid = canvas.width / 2;
             const yMid = canvas.height / 2;
             ctx.lineWidth = lineWidth;
@@ -30,7 +16,20 @@ const PieChart = ({data, width, height, radius, lineWidth, fontSize}) => {
 
             let stepMultiplier = 0.2;
             
-            const drawChart = () => {               
+            const drawChart = () => {
+                ctx.clearRect(0,0,canvas.width, canvas.height);
+
+                // print text
+                ctx.fillstyle = "black";
+                ctx.font = `${fontSize}px sans-serif`;
+                const text = totalRecords.toString();
+                const { width, actualBoundingBoxAscent, actualBoundingBoxDescent } = ctx.measureText(text);
+                const offsetWidth = width / 2;
+                const offsetHeight = (actualBoundingBoxAscent + actualBoundingBoxDescent) / 2;
+                const textX = (canvas.width / 2) - offsetWidth;
+                const textY = (canvas.height / 2) + offsetHeight;
+                ctx.fillText(text, textX, textY);
+
                 let pathStart = -Math.PI / 2;
                 for (let i=0; i<data.length; i++) {
                     const d = data[i];
@@ -40,13 +39,13 @@ const PieChart = ({data, width, height, radius, lineWidth, fontSize}) => {
 
                     ctx.strokeStyle = d.color;
                     ctx.beginPath();                
-                    ctx.arc(xMid, yMid, radius, pathStart + radianGap, pathStart + radians - radianGap);                    
+                    ctx.arc(xMid, yMid, radius, pathStart + radianGap, pathStart + radians - radianGap, false);                    
                     pathStart = pathEnd;
                     ctx.stroke();
                 }
 
                 if(stepMultiplier < 1) {
-                    stepMultiplier += 0.06;
+                    stepMultiplier += 0.05;
                     window.requestAnimationFrame(drawChart);
                 }
             }
